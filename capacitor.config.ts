607bab6e555990f36dfb41d6f4d18af1426047f4
Sanjay Duwal <sanzay8342258@gmail.com>
2024-07-08 15:11:38 -0500

    restarting the project

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.adquiz.app',
-  appName: 'ADQuiz',
-  webDir: 'build'
-};
-
-export default config;
